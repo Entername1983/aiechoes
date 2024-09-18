d31f4506b41b47703912d36caaeae228dfc49e52
entername1983 <kevin.e.mccarthy1983@gmail.com>
2024-09-18 18:42:36 +0100

    build: build buuild buiiiild

--- front/src/client/core/OpenAPI.ts
+++ front/src/client/core/OpenAPI.ts
@@ -1,13 +1,12 @@
 /* Copy to be used to replace generated one, do not delete */
 /* istanbul ignore file */
-/* tslint:disable */
 /* eslint-disable */
 
 // Command to use to generate new api
 // npx @hey-api/openapi-ts -i http://localhost:5000/openapi.json -o src/client -c @hey-api/client-fetch
 
 import type { ApiRequestOptions } from "./ApiRequestOptions";
-const backendUrl = import.meta.env.VITE_BACKEND_URL;
+const backendUrl = "/api";
 
 type Resolver<T> = (options: ApiRequestOptions) => Promise<T>;
 type Headers = Record<string, string>;
