016fb3e6aa2bd7d31f3fa6d76bef46b926d81bba
entername1983 <kevin.e.mccarthy1983@gmail.com>
2024-09-18 09:09:34 +0100

    build: moved some hard coded sentry sentings to env

--- back/app/main.py
+++ back/app/main.py
@@ -1,25 +1,26 @@
 import sentry_sdk
 from fastapi import FastAPI
 
+from app.dependencies.settings import get_settings
 from app.startup.startup import create_app
 
+settings = get_settings()
+
 sentry_sdk.init(
-    dsn="https://78859ae67c9ee114c055c4349364e28c@o4506774392078336.ingest.us.sentry.io/4507526319505408",
-    traces_sample_rate=1.0,
-    profiles_sample_rate=1.0,
+    dsn=settings.monitoring.sentry_dsn,
+    traces_sample_rate=settings.monitoring.traces_sample_rate,
+    profiles_sample_rate=settings.monitoring.profiles_sample_rate,
 )
 
 
 app: FastAPI = create_app()
 
 
-
-
 @app.get("/")
-async def root():
+async def root() -> dict:
     return {"message": "Hello World"}
 
 
 @app.get("/healthcheck")
-async def healthcheck():
+async def healthcheck() -> dict:
     return {"message": "Hello World"}
