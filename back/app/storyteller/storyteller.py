66b66d6df7a71ef68a65b014ec2e4af42e698f49
entername1983 <kevin.e.mccarthy1983@gmail.com>
2024-09-18 18:50:32 +0100

    build: build buuild buiiiild

--- back/app/storyteller/storyteller.py
+++ back/app/storyteller/storyteller.py
@@ -28,7 +28,7 @@ log = logging.getLogger("Storyteller")
 
 scheduler = AsyncIOScheduler()
 
-trigger = CronTrigger(second="10")
+trigger = CronTrigger(second=settings.story.interval_min * 60)
 
 
 scheduler.add_job(job_function, trigger)
